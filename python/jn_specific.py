fe5fcf618282b2379e97364758431942d4ce535f
zhehaowang <wangzhehao410305@gmail.com>
2016-12-17 20:33:26 -0800

    initial commit: source, string parsing, pages

---
+++ python/jn_specific.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+
+from string_parser import StringParse
+
+#def lianhua_recipe():
+
+class JnSpecific():
+    def __init__(self):
+        self.script_file_path = "../src/disaster4script.j"
+        self.string_parser = StringParse()
+        self.item_class_order = ["S+", "S", "A+", "A", "B+", "B", "C+", "C", "D+", "D", "E+", "E", "F+", "F", "G+", "G"]
+        
+        self.item_magic_stone = []
+        self.item_regular = []
+        self.item_misc = []
+
+        self.html_template_start = "<div id=\"main_content_wrap\" class=\"outer\">\n<section id=\"main_content\" class=\"inner\">\n"
+        self.html_template_end = "\n</section>\n</div>"
+
+        return
+
+    def parse_string(self):
+        self.string_parser.parse()
+        return
+
+    def classify_items(self):
+        classified_cnt = 0
+        stone_cnt = 0
+        item_dict = self.string_parser.item_id_dict
+        for item in item_dict:
+            item_dict[item]["item_id"] = item
+            if not "description" in item_dict[item]:
+                print "item " + item_dict[item]["name"] + "(" + item + ") does not have a description"
+                continue
+
+            description = item_dict[item]["description"]
+
+            # magic stones
+            if "魔石" in description:
+                item_dict[item]["jn_type"] = "stone"
+                self.item_magic_stone.append(item_dict[item])
+                stone_cnt += 1
+            else:
+                # TODO: add specific type association Trig_item_wuqiActions, item types (weapon / armor) are done via item health
+                item_dict[item]["jn_type"] = "regular"
+                groups = re.search(r"等级：([A-Z]\+?)", description)
+                if groups:
+                    this_class = groups.group(1)
+                    item_dict[item]["jn_class"] = this_class
+                    self.item_regular.append(item_dict[item])
+                    classified_cnt += 1
+                else:
+                    print "item " + item_dict[item]["name"] + "(" + item + ") does not have a class"
+                    self.item_misc.append(item_dict[item])
+        print "found " + str(stone_cnt) + " magic stones"
+        print "classified " + str(classified_cnt) + " items"
+        print "unable to process " + str(len(item_dict) - stone_cnt - classified_cnt) + " items"
+
+    def sort_order(self, v):
+        if 'jn_class' in v:
+            return self.item_class_order.index(v['jn_class'])
+        else:
+            raise ValueError("Regular item does not have classification: " + str(v))
+
+    def sort_items(self):
+        self.item_regular.sort(key = self.sort_order)
+
+    def beautify_escaped_string_html(self, s):
+        s = re.sub(r"\|n", "<br>", s)
+        #s = re.sub(r"\|cff([0-9a-zA-z]{6})([^\|]*)\|r", r"<font color=#\1>\2</font>", s)
+        s = re.sub(r"\|cff([0-9a-zA-z]{6})", r"<font color=#\1>", s)
+        s = re.sub(r"\|r", r"</font>", s)
+        return s
+
+    def regular_items_to_html(self):
+        html_string = ""
+        for item in self.item_regular:
+            html_string += "<h3 id=\"it-" + item["item_id"] + "\">" + self.beautify_escaped_string_html(item["name"]) + "</h3>\n"
+            # html_string += "<p><b>等级 </b>" + self.beautify_escaped_string_html(item["jn_class"]) + "</p>\n"
+            html_string += "<p><b>" + self.beautify_escaped_string_html(item["description"]) + "<b></p>\n<br>\n"
+            # html_string += "<p><b>掉落/相关触发</b>" + item["class"] + "</p>\n"
+        return html_string
+
+    def write_html_to_file(self, html_string, file_name):
+        html_string = self.html_template_start + html_string + self.html_template_end
+        f = open(file_name,'w')
+        f.write(html_string)
+        f.close()
+
+if __name__ == "__main__":
+    jn = JnSpecific()
+    jn.parse_string()
+    jn.classify_items()
+    jn.sort_items()
+    jn.write_html_to_file(jn.regular_items_to_html(), "../docs/items-content.html")
---
+++ python/string_parser.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+
+class StringParse():
+    def __init__(self):
+        self.string_file_path = "../src/disaster4strings.wts"
+        self.item_id_dict = dict()
+        self.unit_id_dict = dict()
+        self.spell_id_dict = dict()
+        self.spell_effect_id_dict = dict()
+        self.tech_id_dict = dict()
+        self.destructable_id_dict = dict()
+        self.untyped_string_dict = dict()
+        self.module = "string_parsing"
+
+    def log_error(self, line_number, content):
+        print self.module + ":" + str(line_number) + " " + content
+
+    def dict_insertion(self, dst_dict, this_id, key, value, line_number = 0, expand_duplicate = False):
+        if this_id in dst_dict:
+            if key in dst_dict[this_id]:
+                if not expand_duplicate:
+                    self.log_error(str(line_number), "key already exists: " + key + " in " + this_id)
+                else:
+                    if not isinstance(dst_dict[this_id][key], list):
+                        dst_dict[this_id][key] = [dst_dict[this_id][key]]
+                    dst_dict[this_id][key].append(value)
+            else:
+                dst_dict[this_id][key] = value
+        else:
+            dst_dict[this_id] = {}
+            dst_dict[this_id][key] = value
+
+    def parse(self):
+        with open(self.string_file_path, "r") as jn_strings:
+            idx = -1
+            line_number = 0
+            in_brackets = False
+
+            this_id = ""
+            this_type = ""
+            this_descriptor = ""
+            this_name = ""
+            this_description = ""
+            this_level = ""
+            str_id = ""
+
+            for line in jn_strings:
+                line_number += 1
+                idx += 1
+                if (not in_brackets) and re.match(r"\s", line):
+                    idx = -1
+                    this_id = ""
+                    this_type = ""
+                    this_descriptor = ""
+                    this_name = ""
+                    this_description = ""
+                    this_level = ""
+                    str_id = ""
+                else:
+                    if idx == 0:
+                        groups = re.match(r"STRING ([0-9]+)\s", line)
+                        if groups:
+                            str_id = groups.group(1)
+                        else:
+                            self.log_error(line_number, "cannot match string ID pattern: " + line)
+                    elif idx == 1:
+                        if re.match("{\s", line):
+                            in_brackets = True
+                            continue
+                        else:
+                            groups = re.match(r"// (.*): ([a-zA-Z0-9]{4}) (\(.*\)), (.*) \(", line)
+                            this_type = ""
+                            if groups:
+                                this_type = groups.group(1)
+                                this_id = groups.group(2)
+                                this_name = groups.group(3)
+                                this_descriptor = groups.group(4)
+                                idx -= 1
+                            else:
+                                self.log_error(line_number, "unexpected string: " + line)
+                    elif idx == 2:
+                        this_description = line
+                    else:
+                        if re.match("}\s", line):
+                            if this_type == "单位":
+                                self.dict_insertion(self.unit_id_dict, this_id, this_descriptor.lower(), this_description.strip(), line_number)
+                            elif this_type == "物品":
+                                self.dict_insertion(self.item_id_dict, this_id, this_descriptor.lower(), this_description.strip(), line_number)
+                            elif this_type == "技能":
+                                self.dict_insertion(self.spell_id_dict, this_id, this_descriptor.lower(), this_description.strip(), line_number, True)
+                            elif this_type == "可破坏物":
+                                self.dict_insertion(self.destructable_id_dict, this_id, this_descriptor.lower(), this_description.strip(), line_number)
+                            elif this_type == "魔法效果":
+                                self.dict_insertion(self.spell_effect_id_dict, this_id, this_descriptor.lower(), this_description.strip(), line_number)
+                            elif this_type == "科技":
+                                self.dict_insertion(self.tech_id_dict, this_id, this_descriptor.lower(), this_description.strip(), line_number, True)
+                            elif this_type == "":
+                                self.dict_insertion(self.untyped_string_dict, str_id, "content", this_description.strip(), line_number)
+                            else:
+                                self.log_error(line_number, "untyped: " + this_description)
+                            in_brackets = False
+                        else:
+                            this_description += line
+            print "processed: " + str(len(self.unit_id_dict)) + " units"
+            print "processed: " + str(len(self.item_id_dict)) + " items"            
+            print "processed: " + str(len(self.spell_id_dict)) + " spells"
+            print "processed: " + str(len(self.spell_effect_id_dict)) + " spell effects"
+            print "processed: " + str(len(self.tech_id_dict)) + " techs"
+            print "processed: " + str(len(self.destructable_id_dict)) + " destructables"
+            print "processed: " + str(len(self.untyped_string_dict)) + " untyped strings"
+
+def pretty_print(this_dict):
+    for key in this_dict:
+        print "id: " + key
+        for subkey in this_dict[key]:
+            if isinstance(this_dict[key][subkey], list):
+                print "  * " + subkey
+                for i in range(0, len(this_dict[key][subkey])):
+                    print "    * lv" + str(i + 1) + " : " + str(this_dict[key][subkey][i])
+            else:
+                print "  * " + subkey + " : " + this_dict[key][subkey]
+
+if __name__ == "__main__":
+    string_parser = StringParse()
+    string_parser.parse()
+    pretty_print(string_parser.item_id_dict)
+    #pretty_print(string_parser.unit_id_dict)
+    #pretty_print(string_parser.spell_id_dict)
+    #pretty_print(string_parser.tech_id_dict)
+
+    
+
